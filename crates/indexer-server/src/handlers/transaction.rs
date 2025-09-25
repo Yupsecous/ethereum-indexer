193d888a4e6e2acbeac1134816d9947b8a76f316
Yupsecous <zxweb8895@gmail.com>
2025-09-25 15:58:18 +0200

    chore: run cargo fmt

--- crates/indexer-server/src/handlers/transaction.rs
+++ crates/indexer-server/src/handlers/transaction.rs
@@ -1,3 +1,4 @@
+use alloy::primitives::B256;
 use axum::{
     extract::{Path, State},
     http::StatusCode,
@@ -7,7 +8,6 @@ use futures::StreamExt;
 use indexer::{EthereumIndexer, TxByHashBuilder, TxByHashPlan};
 use std::sync::Arc;
 use tracing::info;
-use alloy::primitives::B256;
 
 pub async fn get_transaction_by_hash(
     State(engine): State<Arc<EthereumIndexer>>,
